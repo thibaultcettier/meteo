6761f02ab25eb55f94c5183d9aa38955709555aa
Thibault <thibault_cettier@hotmail.fr>
2024-07-03 16:17:56 +0200

    implement header & footer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { importProvidersFrom } from '@angular/core';
+import { HeaderComponent } from './app/assets/header/header.component';
+import { FooterComponent } from './app/assets/footer/footer.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      importProvidersFrom(BrowserModule),
+      HeaderComponent,
+      FooterComponent
+    ]
+  }).catch(err => console.error(err));
